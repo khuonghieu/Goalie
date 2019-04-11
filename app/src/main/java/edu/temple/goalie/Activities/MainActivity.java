e7a1009dfb5246edbefd29db347378eb5b8d5579
Hieu Khuong <tug60121@temple.edu>
2019-04-11 17:00:16 -0400

    Minor modification.

--- app/src/main/java/edu/temple/goalie/Activities/MainActivity.java
+++ app/src/main/java/edu/temple/goalie/Activities/MainActivity.java
@@ -3,19 +3,15 @@ package edu.temple.goalie.Activities;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
-
-import java.util.ArrayList;
 
 import edu.temple.goalie.Database.DBHelper;
 import edu.temple.goalie.R;
