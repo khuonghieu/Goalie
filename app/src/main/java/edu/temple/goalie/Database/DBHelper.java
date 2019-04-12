3f055d8836b141aaf748b46e708dc7a738b39226
Hieu Khuong <tug60121@temple.edu>
2019-04-12 14:10:43 -0400

    Merged current master branch into merged_into_one_app. Fixed merge conflict.

--- app/src/main/java/edu/temple/goalie/Database/DBHelper.java
+++ app/src/main/java/edu/temple/goalie/Database/DBHelper.java
@@ -3,7 +3,6 @@ package edu.temple.goalie.Database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.EventLogTags;
 
 public class DBHelper extends SQLiteOpenHelper {
 
